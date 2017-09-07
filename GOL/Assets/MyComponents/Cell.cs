8480c86a25cf542bdc9bdee4a72e27f2bfda9ab2
timomak <tmakhlay2@gmail.com>
2017-09-07 16:04:12 -0700

    Updates

--- GOL/Assets/MyComponents/Cell.cs
+++ GOL/Assets/MyComponents/Cell.cs
@@ -2,7 +2,8 @@ using UnityEngine;
 using System.Collections;
 
 public class Cell : MonoBehaviour {
-
+	private bool isAlive;
+	float scaleFactor;
     // Use this for initializatio
     void Start() {
 
@@ -10,6 +11,12 @@ public class Cell : MonoBehaviour {
     
     // Update is called once per fram
     void Update() {
-
+		if (isAlive == true) {
+			scaleFactor = 1;
+			Utilities.ChangeCellColor (this, Color.green);
+		} else {
+			scaleFactor = 0.5f;
+			Utilities.ChangeCellColor (this, Color.red);
+		}
     }
 }
--- GOL/Assets/MyComponents/Grid.cs
+++ GOL/Assets/MyComponents/Grid.cs
@@ -7,15 +7,17 @@ public class Grid : MonoBehaviour {
 	private int numRows = 5;
 	private float cellSideLenght = 1;
 	private float margin = 0.5f;
-
+	private Cell[,] cells;
 	// Use this for initialization
 	void Start() {
+		cells = new Cell[numCols, numRows];
 		for (int col = 0; col < numCols; ++col) {
 			for (int row = 0; row < numRows; ++row) {
 				Cell cell = Utilities.GetNewCell ();
 				float x = (col + 0.5f - numCols * 0.5f) * (cellSideLenght + margin);
 				float y = (row + 0.5f - numRows * 0.5f) * (cellSideLenght + margin);
 				cell.transform.localPosition = new Vector2 (x, y);
+				cells [col, row] = cell;
 			}
 		}				
 	}
