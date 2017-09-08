e27a6dfb8fb66a5985a4ef8ead06b0432ed89e50
timomak <tmakhlay2@gmail.com>
2017-09-07 20:28:34 -0700

    Working code, no GU, no 3d

--- GOL/Assets/MyComponents/Cell.cs
+++ GOL/Assets/MyComponents/Cell.cs
@@ -3,19 +3,29 @@ using System.Collections;
 
 public class Cell : MonoBehaviour {
 	private bool isAlive;
-	float scaleFactor;
+	public bool isAliveNext;
+	private float scaleFactor;
+	public bool IsAlive() {
+		return isAlive;
+	}
+
+	public void UpdateIsAlive() {
+		isAlive = isAliveNext;
+	}
     // Use this for initializatio
     void Start() {
-
+		
     }
     
     // Update is called once per fram
     void Update() {
 		if (isAlive == true) {
 			scaleFactor = 1;
+			transform.localScale = Vector3.one * scaleFactor;
 			Utilities.ChangeCellColor (this, Color.green);
 		} else {
 			scaleFactor = 0.5f;
+			transform.localScale = Vector3.one * scaleFactor;
 			Utilities.ChangeCellColor (this, Color.red);
 		}
     }
--- GOL/Assets/MyComponents/Grid.cs
+++ GOL/Assets/MyComponents/Grid.cs
@@ -1,15 +1,73 @@
 using UnityEngine;
 using System.Collections;
 
-public class Grid : MonoBehaviour {
+public class Grid : MonoBehaviour
+{
 
 	private int numCols = 8;
 	private int numRows = 5;
 	private float cellSideLenght = 1;
 	private float margin = 0.5f;
 	private Cell[,] cells;
+	private float evolutionPeriod = 0.5f;
+	private float evolutionTimer = 0.5f;
+
+	private void Evolve() {
+
+		for (int col = 0; col < cells.GetLength(0); ++col) {
+			for (int row = 0; row < cells.GetLength(1); ++row) {
+
+				int numAliveNeighbors = GetNumAliveNeighbors(col,row);
+
+				Cell cell = cells[col,row];
+
+				if (cell.IsAlive()) {
+
+					if (numAliveNeighbors < 2 || numAliveNeighbors > 3) {
+						cell.isAliveNext = false;
+					} else {
+						cell.isAliveNext = true;
+					}
+
+				} else if (!cell.IsAlive() && numAliveNeighbors == 3) {
+					cell.isAliveNext = true;
+				}
+			}
+		}
+
+		foreach (Cell cell in cells) {
+			cell.UpdateIsAlive();
+		}
+	}
+	private int GetNumAliveNeighbors(int colCenter, int rowCenter) {
+
+		int numAliveNeighbors = 0;
+
+		for (int dCol = -1; dCol <= 1; ++dCol) {
+			for (int dRow = -1; dRow <= 1; ++dRow) {
+
+				if (dCol == 0 && dRow == 0) {continue;}
+
+				int col = colCenter + dCol;
+				int row = rowCenter + dRow;
+
+				if (col < 0) {col = cells.GetLength(0) - 1;}
+				if (col >= cells.GetLength(0)) {col = 0;}
+
+				if (row < 0) {row = cells.GetLength(1) - 1;}
+				if (row >= cells.GetLength(1)) {row = 0;}
+
+				if (cells[col,row].IsAlive()) {
+					++numAliveNeighbors;
+				}
+			}
+		}
+
+		return numAliveNeighbors;
+	}
 	// Use this for initialization
-	void Start() {
+	void Start ()
+	{
 		cells = new Cell[numCols, numRows];
 		for (int col = 0; col < numCols; ++col) {
 			for (int row = 0; row < numRows; ++row) {
@@ -19,35 +77,51 @@ public class Grid : MonoBehaviour {
 				cell.transform.localPosition = new Vector2 (x, y);
 				cells [col, row] = cell;
 			}
-		}				
+		}	
+		cells [2, 3].isAliveNext = true;
+		cells [3, 2].isAliveNext = true;
+		cells [4, 4].isAliveNext = true;		
+		cells [4, 2].isAliveNext = true;
+		cells [4, 3].isAliveNext = true;
 	}
     
 	// Update is called once per fram
-	void Update() {
+	void Update ()
+	{
+		evolutionTimer -= Time.deltaTime;
+		if (evolutionTimer < 0) {
+			evolutionTimer = evolutionPeriod;
+			Evolve ();
+		}
+	}
+
+	public void PlayOrPause ()
+	{
 		
 	}
 
-	public void PlayOrPause() {
+	public void Clear ()
+	{
+
+	}
+
+	public void Randomize ()
+	{
 		
 	}
 
-	public void Clear() {
-
+	public int GetPopulationCount ()
+	{
+		return 0;
 	}
 
-	public void Randomize() {
-		
+	public int GetGenerationCount ()
+	{
+		return 0;
 	}
 
-	public int GetPopulationCount() {
-        	return 0;
-	}
-
-	public int GetGenerationCount() {
-        	return 0;
-	}
-	
-	public void SetEvolutionPeriod(float sliderValue) {
+	public void SetEvolutionPeriod (float sliderValue)
+	{
 
 	}
 }
